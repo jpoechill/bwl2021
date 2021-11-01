70419fc601876f8014076916c68563532851608b
jpoechill <poamrong@rith.com>
2021-10-31 19:10:40 -0700

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,11 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-2 bg-white overflow-hidden p-6">
+        <img src="bwl_splash_103121.svg" alt="" style="width: 100%;">
+      </div>
+    </div>
+  </div>
+</template>
